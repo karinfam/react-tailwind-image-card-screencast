e7e7f487a346f7202c4cf9d3fd39f882d6f1c342
Karin Fam <karinfam@outlook.com>
2020-04-03 00:43:24 +0800

    completed screencast

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { PropertyCard } from './components/PropertyCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const property = {
+		imageUrl:
+			'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+		imageAlt: 'Rear view of modern home with pool',
+		beds: 3,
+		baths: 2,
+		title: 'Modern home in city center',
+		priceInCents: 190000,
+		formattedPrice: '$1,900.00',
+		reviewCount: 34,
+		rating: 4
+	};
+	return (
+		<div className="App">
+			<div class="bg-gray-200 min-h-screen p-8 items-center justify-center antialiased text-gray-900">
+				<PropertyCard property={property} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	theme: {
+		extend: {
+			spacing: {
+				'1/2': '50%',
+				'2/3': '66.6666667%',
+				'5/6': '83.3333333%'
+			}
+		}
+	},
+	variants: {},
+	plugins: []
+};
